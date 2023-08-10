fa8f072dad984dafda6a949c1f165077c135b4fb
sandeeptete2020 <sandeep.tete2012@gmail.com>
2023-08-10 22:32:53 +0530

    Update PartFooter.js

--- src/components/app/PartFooter.js
+++ src/components/app/PartFooter.js
@@ -89,8 +89,7 @@ const PartFooter = () => (
             </div>
 
             <div className="Gray">
-                Copyright © {currentYear} Tropley <LinkTrace href="https://qrbtf.com"
-                                 rel="noopener noreferrer" target="_blank">QRBTF</LinkTrace>
+                Copyright © {currentYear} Tropley <LinkTrace href="https://qrbtf.com" rel="noopener noreferrer" target="_blank">QRBTF</LinkTrace>
                 
             </div>
         </div>
